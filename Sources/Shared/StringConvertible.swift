1882fc1d02ed0a9bae3b346f98dc45c9e1ddc26b
Christoffer Winterkvist <christoffer@hyper.no>
2016-04-24 18:57:59 +0200

    Add documentation

--- Sources/Shared/StringConvertible.swift
+++ Sources/Shared/StringConvertible.swift
@@ -1,9 +1,18 @@
+/**
+ A protocol for returning a string representation of self
+ */
 public protocol StringConvertible {
   var string: String { get }
 }
 
+/**
+  A protocol extension on String to make it conform to StringConvertible
+ */
 extension String: StringConvertible {
-  
+
+  /**
+   - Returns: self as string
+  */
   public var string: String {
     return self
   }
--- Sources/Shared/ViewConfigurable.swift
+++ Sources/Shared/ViewConfigurable.swift
@@ -1,4 +1,9 @@
 public protocol ViewConfigurable: class {
 
+  /**
+   A configure method that is used on reference types that can be configured using a view model
+
+   - Parameter item: A inout ViewModel so that the ViewConfigurable object can configure the view model width and height based on its UI components
+  */
   func configure(inout item: ViewModel)
 }
