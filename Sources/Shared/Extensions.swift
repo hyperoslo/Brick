7216bdebb69084e9635ebdfdc94c4bc4bbb45b95
Vadym Markov <markov.vadym@gmail.com>
2016-04-25 23:23:56 +0200

    Add some documentation

--- Sources/Shared/Extensions.swift
+++ Sources/Shared/Extensions.swift
@@ -11,12 +11,25 @@ public extension _ArrayType where Generator.Element == ViewModel {
 
 // MARK: - Dictionary
 
+/**
+ A dictinary extension to work with custom Key type
+ */
 extension Dictionary where Key: StringLiteralConvertible {
 
+  /**
+   - Parameter name: The name of the property that you want to map
+   - Returns: A generic type if casting succeeds, otherwise it returns nil
+   */
   func property<T>(name: ViewModel.Key) -> T? {
     return property(name.string)
   }
 
+  /**
+   Access the value associated with the given key.
+
+   - Parameter key: The key associated with the value you want to get
+   - Returns: The value associated with the given key
+   */
   subscript(key: ViewModel.Key) -> Value? {
     set(value) {
       guard let key = key.string as? Key else { return }
