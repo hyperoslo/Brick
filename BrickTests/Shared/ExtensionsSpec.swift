d37bbc9c54e6e5e8ead2e652edac4d09e6b17e05
Vadym Markov <markov.vadym@gmail.com>
2016-04-25 23:43:31 +0200

    Add tests for dictionary extension

--- BrickTests/Shared/ExtensionsSpec.swift
+++ BrickTests/Shared/ExtensionsSpec.swift
@@ -7,7 +7,7 @@ class ExtensionsSpec: QuickSpec {
 
   override func spec() {
     describe("Array+Brick") {
-      var items =  [ViewModel]()
+      var items = [ViewModel]()
 
       beforeEach {
         for index in 0..<10 {
@@ -29,6 +29,25 @@ class ExtensionsSpec: QuickSpec {
           for index in 0..<10 {
             expect(items[index].index).to(equal(index))
           }
+        }
+      }
+    }
+
+    describe("Dictionary+Brick") {
+      var items = [String: String]()
+
+      beforeEach {
+        items = ["title": "test"]
+      }
+
+      describe("#subscript") {
+        it("sets a proper value based on a string representation of a view model Key") {
+          items[.Subtitle] = "subtitle"
+          expect(items["subtitle"]).to(equal("subtitle"))
+        }
+
+        it("returns a proper value based on a string representation of a view model Key") {
+          expect(items[.Title]).to(equal("test"))
         }
       }
     }
